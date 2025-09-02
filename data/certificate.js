dc5da2daabde96c4c3d5d94f47881160e3b70546
Unknwonnay <aidonayaka4@gmail.com>
2025-09-02 22:46:23 +0700

    Final

---
+++ data/certificate.js
@@ -0,0 +1,14 @@
+const certificates = [
+    { title: 'Belajar Dasar Cloud dan Gen AI di AWS',
+      institution: 'dicoding',
+      certificates_img : '../assets/certificate-1.png',
+      logo_img: '../assets/dicoding-logo.png'
+    },
+    { title: 'Belajar Membuat Aplikasi Back-End untuk Pemula',
+      institution: 'dicoding',
+      certificates_img : '../assets/certificate-2.png',
+      logo_img: '../assets/dicoding-logo.png'
+    }
+]
+
+export default certificates
---
+++ data/projects.js
@@ -0,0 +1,10 @@
+const projects = [
+  { title: 'E-Perpus', language: 'PHP', color: '#4F5D95' },
+  { title: 'Todo API', language: 'Node.js', color: '#8CC84B' },
+  { title: 'Portfolio', language: 'React', color: '#61DAFB' },
+  { title: 'Cafe Landing', language: 'HTML/CSS', color: '#E34C26' },
+  { title: 'E-Perpus Admin', language: 'PHP', color: '#4F5D95' },
+  { title: 'Weather App', language: 'JavaScript', color: '#F7DF1E' },
+];
+
+export default projects
